4ed2ec38cf2b1ed542640f1d98ebd98794188877
ivl-ad <i@ivl.ad>
2026-09-15 21:01:39 -0400

    Partners: 6 logos, edited in the web editor by Greg

--- js/partners.js
+++ js/partners.js
@@ -14,6 +14,9 @@
  */
 window.PARTNERS = [
   {"name": "down2chill", "url": "https://down2chill.com", "logo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgWbd5u_F9CxHjvuwypPjEuf8SEymU0Jj7lcRKucA2VA&s=10", "note": "delete this once we have real ones"},
-  {"name": "macys", "url": "https://macys.com", "logo": "https://cdn.mos.cms.futurecdn.net/kPTwCmCKYJUwGbDbRZr9MX-650-80.png.webp", "note": "delete this once we have real ones"},
-  {"name": "macys (copy)", "url": "https://nike.com", "logo": "https://1000logos.net/wp-content/uploads/2021/11/Nike-Logo.jpg", "note": "delete this once we have real ones"},
+  {"name": "Vans", "url": "https://vans.pxf.io/c/5891219/3950948/48802", "logo": "https://www.vans.com/favicon.ico"},
+  {"name": "Fanatics", "url": "https://fanatics.93n6tx.net/9VZnM0", "logo": "https://www.fanatics.com/content/ws/all/b88152dc-ebb5-4bbb-a3fe-fe3b2f15de08.svg"},
+  {"name": "Belkin", "url": "https://belkin.evyy.net/YVY9nO", "logo": "https://www.belkin.com/on/demandware.static/Sites-BELKUS-Site/-/default/dw39bf336a/images/brand/belkin_logo_notm.svg"},
+  {"name": "Gemini", "url": "https://gemini.sjv.io/1GkNbz", "logo": "https://www.gemini.com/favicon.ico"},
+  {"name": "NFL Shop", "url": "https://nflshop.k77v.net/X4Re13", "logo": "https://www.nflshop.com/content/assets/__0-859256003070.1062.svg"},
 ];
